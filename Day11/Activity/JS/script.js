23b2878302f47b8c305687ad4c8b5f41b7f80b67
Shrouq Ramadan <your-email@example.com>
2026-08-24 08:55:18 +0300

    Add Activity

---
+++ Day11/Activity/JS/script.js
@@ -0,0 +1,23 @@
+let number = -5;
+
+// Positive or Negative checker
+
+if (number > 0) {
+    console.log("Positive number");
+}
+else if (number < 0) {
+    console.log("Negative number");
+}
+
+else {
+    console.log("number is 0");
+}
+
+//Even or Odd checker
+
+if (number % 2 == 0){
+    console.log("Even number")
+}
+else if (number % 2 != 0){
+    console.log("Odd number")
+}
